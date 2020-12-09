5be046580f20326f8af6e7179aeae16c00f7491a
BMatischen <benito.matischen01@gmail.com>
2020-12-09 23:57:56 +0000

    First half done

--- AOC1.py
+++ AOC1.py
@@ -2,6 +2,19 @@ numbers = []
 with open("input.txt", "rt") as input_handler:
     for n in input_handler.readlines():
         numbers.append(int(n.strip()))
-print(numbers)
+#print(numbers)
+
+goal = 2020
+target_found = False
+for i in numbers:
+    if target_found:
+        break
+    target = goal - i
+    print("Current_num: ", i, "Target: ", target, "\n")
+    for j in range(len(numbers)):
+        if numbers[j] == target:
+            target_found = True
+            print(i * numbers[j])
+            break
 
 
