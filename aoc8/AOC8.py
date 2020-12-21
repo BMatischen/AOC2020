50b6986c4e6c03bfc25e44f8252c662a6b2932ce
BMatischen <benito.matischen01@gmail.com>
2020-12-21 21:44:38 +0000

    Second part of puzzle 8

--- aoc8/AOC8.py
+++ aoc8/AOC8.py
@@ -30,15 +30,14 @@ def get_jump_offset(instr):
 
 
 def run_program(instr_list):
+    global acc
+    acc = 0
     executed = []
     i = 0
     while i < len(instr_list):
-        print(i, instr_list[i])
-        print("ACC: ", acc, "\n")
         if i in executed:
-            print("REPEATED!")
             print("ACC before repeat: ", acc)
-            break
+            return False
         else:
             executed.append(i)
 
@@ -50,7 +49,34 @@ def run_program(instr_list):
         else:
             i += get_jump_offset(instr_list[i])
     print("Final ACC:", acc)
+    return True
 
+
+def fix_program():
+    changed_lines = set({})
+    index = 0
+    while True:
+        instr_list = get_instructions()
+        fixed = False
+        if index in changed_lines:
+            index += 1
+        elif index >= len(instr_list):
+            print(acc)
+
+        else:
+            if "jmp" in instr_list[index]:
+                instr_list[index] = re.sub("jmp","nop",instr_list[index])
+                changed_lines.add(index)
+                fixed = run_program(instr_list)
+            elif "nop" in instr_list[index]:
+                instr_list[index] = re.sub("nop","jmp",instr_list[index])
+                changed_lines.add(index)
+                fixed = run_program(instr_list)
+            else:
+                index += 1
+        if fixed:
+            break
+            
 
 def part_one():
     instructs = get_instructions()
@@ -58,11 +84,14 @@ def part_one():
     run_program(instructs)
 
 
+def part_two():
+    fix_program()
+
+
 def main():
     part_one()
+    part_two()
 
 
 if __name__ == "__main__":
-    global acc
-    acc = 0
     main()
